cb554e6b711630d30ae777412ddba520d122c4f8
krish-coder-24 <127621879+krish-coder-24@users.noreply.github.com>
2023-09-02 20:40:22 +0530

    Update files.py

--- files.py
+++ files.py
@@ -1,6 +1,28 @@
 class get:
    def help():
-     print ("Help")
+     print ("""
+     Penda Version - v2.67
+
+     Penda is Official Programming language created by Krish Anjara 
+
+     Usage: penda [ -h | --help] [ -V | --version ] [ -f | --file ] < FILE > [ -U | --update ]
+                  [ --uninstall ] 
+
+     Short  |   Long        Description...
+
+     -f     |   --file      Requires a file written in penda script
+                            To execute.
+                            
+     -V     |   --version   Prints the version of program
+
+     -U     |   --update    Updates to the latest version Of Penda
+
+     -h     |   --help      Prints this help message
+     
+      """)   
 
    def version():
-     print ("Penda version(): v2.67")
+     print ("""
+             Penda Version: v2.67
+             Penda is Official Programming language created by Krish Anjara
+             """) 
